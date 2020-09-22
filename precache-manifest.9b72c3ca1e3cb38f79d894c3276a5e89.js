a0bb7366d9b1bc224652f614c66c0d428282bfdd
Alan Pallath <iamakidilam@gmail.com>
2020-09-23 00:32:31 +0930

    Updates

---
+++ precache-manifest.9b72c3ca1e3cb38f79d894c3276a5e89.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2fb1c0b9f36fbda64ce1e09166b3a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "078d2aaeada1b783db2f",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "47eb0dbb7bb42f4e77f8",
+    "url": "/static/css/main.81444521.chunk.css"
+  },
+  {
+    "revision": "078d2aaeada1b783db2f",
+    "url": "/static/js/2.0cf08d0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0cf08d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47eb0dbb7bb42f4e77f8",
+    "url": "/static/js/main.4640f8a2.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "1b0b504d652a49d19f38b594be7ac028",
+    "url": "/static/media/alan.1b0b504d.jpg"
+  },
+  {
+    "revision": "4b638a4c509ec7e41f507499a3ce6e14",
+    "url": "/static/media/pigeon.4b638a4c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ebfa825472717b8c1e23ed6584187fd4.js"
+  "/precache-manifest.9b72c3ca1e3cb38f79d894c3276a5e89.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
